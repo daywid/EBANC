a2568d780c3944677608a234ff8f2099b80d8535
Daywid <88506180+daywid@users.noreply.github.com>
2023-06-14 22:19:24 -0300

    update!

--- jornadadev/src/pages/video.js
+++ jornadadev/src/pages/video.js
@@ -1,17 +1,31 @@
-import React from 'react'
-import "./video.css"
+import React, { useRef, useState } from "react";
+import "./video.css";
 
 function Video() {
+  const videoRef = useRef(null);
+  const [play, setPlay] = useState(false)
+
+  function handdleStart() {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
+    }
+  }
+
   return (
-    <div className ='video'>
-        Eu sou um componente video
-        <video 
-            className='video__player'
-            src='https://www.tiktok.com/@china_mew/video/7200398672016788782?is_from_webapp=1'
-        >
-        </video>
+    <div className="video">
+      <video
+        className="video__player"
+        ref={videoRef}
+        onClick={handdleStart}
+        loop
+        src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+      ></video>
     </div>
-  )
+  );
 }
 
-export default Video
+export default Video;
