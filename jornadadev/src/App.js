fa6521db371567c6143607637b83b0ab38a55995
Daywid <88506180+daywid@users.noreply.github.com>
2023-06-17 20:26:01 -0300

    incrementing database

--- jornadadev/src/App.js
+++ jornadadev/src/App.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import Video from "./pages/video"
+import Video from "./pages/video";
+import db from "./config/firebase";
+import { collection, getDocs } from "firebase/firestore/lite";
 
 function App() {
+  const [video, setVideos] = useState([]);
+
+  async function getVideos() {
+    const videosCollection = collection(db, "videos");
+    const videosSnapshot = await getDocs(videosCollection);
+    const videosList = videosSnapshot.docs.map((doc) => doc.data());
+    setVideos(videosList);
+  }
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
   return (
     <div className="App">
-     <div className ="app__videos">
-      <Video 
-        likes={500}
-        messages={200}
-        shares={100}
-        name="Dayyy"
-        description="gatin goleiro!"
-        music="musica epica!!"
-        url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
-      />
-      <Video 
+      <div className="app__videos">
+        {video.map((item) => {
+          return (
+            <Video
+              likes={item.likes}
+              messages={item.messages}
+              shares={item.shares}
+              name={item.name}
+              description={item.description}
+              music={item.music}
+              url={item.url}
+            />
+          );
+        })}
+
+        {/* <Video 
         likes={449}
         messages={180}
         shares={90}
@@ -23,8 +43,8 @@ function App() {
         description="gatin!"
         music="Clap your hands"
         url= "https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4?t=2023-05-22T19%3A40%3A47.052Z"
-        />
-     </div>
+        /> */}
+      </div>
     </div>
   );
 }
