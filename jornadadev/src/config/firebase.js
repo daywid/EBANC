73ac9ff0888fe5ed3154588ef07731926dbd53d1
Daywid <88506180+daywid@users.noreply.github.com>
2023-06-19 14:50:35 -0300

    Update firebase.js

--- jornadadev/src/config/firebase.js
+++ jornadadev/src/config/firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore} from 'firebase/firestore/lite';
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration!!
 const firebaseConfig = {
-  apiKey: "AIzaSyCaLIgVtMcxTMde8onpSYMrjUTD3-xQODA",
-  authDomain: "tiktok---daywid.firebaseapp.com",
-  projectId: "tiktok---daywid",
-  storageBucket: "tiktok---daywid.appspot.com",
-  messagingSenderId: "248914989237",
-  appId: "1:248914989237:web:41de8b62debc48d78ceefc"
+  apiKey: "APIKEY",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
